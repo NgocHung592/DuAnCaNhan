63c0d80dda0b6b973b73e713707bbfd436e0c193
TIPSPC\Hung <ngochung5923@gmail.com>
2023-07-04 11:33:20 +0700

    san pham

--- frontend/src/route.js
+++ frontend/src/route.js
@@ -2,8 +2,9 @@ var myApp = angular.module("myApp", ["ngRoute"]);
 
 myApp.config(function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix("");
+
   $routeProvider.when("/san-pham/hien-thi", {
-    templateUrl: "page/hien-thi-san-pham.html",
+    templateUrl: "pages/hien-thi-san-pham.html",
     controller: SanPhamController,
   });
 });
