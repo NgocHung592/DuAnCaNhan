659f19adff7302d8b63bb2580e4eb8cbead9cd0b
TIPSPC\Hung <ngochung5923@gmail.com>
2023-07-06 18:51:14 +0700

    crud san pham

--- frontend/src/controller/SanPhamController.js
+++ frontend/src/controller/SanPhamController.js
@@ -1,9 +1,52 @@
-window.SanPhamController = function ($http, $scope) {
+window.SanPhamController = function ($http, $scope, $location) {
   $scope.listSanPham = [];
-
-  //load table
+  $scope.formProduct = {
+    id: "",
+    ma: "",
+    ten: "",
+    moTa: "",
+    trangThai: Number,
+  };
+  //get all
   $http.get(sanPhamAPI + "/hien-thi").then(function (response) {
     $scope.listSanPham = response.data;
-    console.log(listSanPham);
+    console.log($scope.listSanPham);
   });
+  //detai san pham
+  $scope.detail = function (id) {
+    $http.get(sanPhamAPI + "/detail/" + id).then(function (response) {
+      if (response.status == 200) {
+        $scope.formProduct = response.data;
+      }
+    });
+  };
+  //delete san pham
+  $scope.delete = function (id) {
+    $http.delete(sanPhamAPI + "/delete/" + id).then(function (response) {
+      $http.get(sanPhamAPI + "/hien-thi").then(function (response) {
+        $scope.listSanPham = response.data;
+      });
+    });
+  };
+  //add san pham
+  $scope.add = function () {
+    $http
+      .post(sanPhamAPI + "/add", $scope.formProduct)
+      .then(function (response) {
+        $http.get(sanPhamAPI + "/hien-thi").then(function (response) {
+          $scope.listSanPham = response.data;
+        });
+      });
+  };
+
+  //update san pham
+  $scope.update = function (id) {
+    $http
+      .put(sanPhamAPI + "/update/" + id, $scope.formProduct)
+      .then(function (response) {
+        $http.get(sanPhamAPI + "/hien-thi").then(function (response) {
+          $scope.listSanPham = response.data;
+        });
+      });
+  };
 };
