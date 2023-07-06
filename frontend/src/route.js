1393aa98c243f04fc0b12b8e6ce18e2301a14780
TIPSPC\Hung <ngochung5923@gmail.com>
2023-07-07 01:27:08 +0700

    sua crud frontend

--- frontend/src/route.js
+++ frontend/src/route.js
@@ -5,12 +5,40 @@ myApp.config(function ($routeProvider, $locationProvider) {
 
   $routeProvider
     .when("/san-pham/hien-thi", {
-      templateUrl: "pages/hien-thi-san-pham.html",
+      templateUrl: "hien-thi-san-pham.html",
       controller: SanPhamController,
     })
-    .when("/admin", {
-      templateUrl: "pages/admin.html",
+    .when("/danh-muc/hien-thi", {
+      templateUrl: "hien-thi-danh-muc.html",
+      controller: SanPhamController,
     })
+    .when("/mau-sac/hien-thi", {
+      templateUrl: "hien-thi-mau-sac.html",
+      controller: SanPhamController,
+    })
+    .when("/kich-thuoc/hien-thi", {
+      templateUrl: "hien-thi-kich-thuoc.html",
+      controller: SanPhamController,
+    })
+    .when("/chat-lieu/hien-thi", {
+      templateUrl: "hien-thi-chat-lieu.html",
+      controller: SanPhamController,
+    })
+    .when("/hoa-tiet/hien-thi", {
+      templateUrl: "hien-thi-hoa-tiet.html",
+      controller: SanPhamController,
+    })
+    .when("/phong-cach/hien-thi", {
+      templateUrl: "hien-thi-phong-cach.html",
+      controller: SanPhamController,
+    })
+    .when("/kieu-dang/hien-thi", {
+      templateUrl: "hien-thi-kieu-dang.html",
+      controller: SanPhamController,
+    })
+    // .when("/admin", {
+    //   templateUrl: "admin.html",
+    // })
     .otherwise({
       redirectTo: "/admin",
     });
