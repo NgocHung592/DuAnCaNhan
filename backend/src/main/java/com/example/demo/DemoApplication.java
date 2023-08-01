0442432f0918679c2b58e8d1900cac47a96046a5
TIPSPC\Hung <ngochung5923@gmail.com>
2023-08-01 18:07:13 +0700

    sua tí

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -12,13 +12,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        long vnd = 10000000L;
-        Locale localeVN = new Locale("vi", "VN");
-        NumberFormat currencyVN = NumberFormat.getCurrencyInstance(localeVN);
-        String str1 = currencyVN.format(vnd);
-        System.out.println("Số " + vnd + " sau khi định dạng = " + str1);
-        System.out.println("Kiểu đơn vị tiền tệ của " + localeVN.getCountry() +
-                " là " + currencyVN.getCurrency());
+      
     }
 
 }
