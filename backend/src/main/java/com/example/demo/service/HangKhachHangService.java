e12cd2abe3fa843410d3740b966f604f461a1fc0
unknown <cubervd74@gmail.com>
2023-10-24 22:48:54 +0700

    nhan vien be

--- backend/src/main/java/com/example/demo/service/HangKhachHangService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.HangKhachHang;
-
-import java.util.List;
-
-public interface HangKhachHangService {
-    List<HangKhachHang> getListStatus();
-}
