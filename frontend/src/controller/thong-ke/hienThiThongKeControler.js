5ef2ecfc08ef795049eb02d705d572afb63a2d04
DESKTOP-EBP024J\ASUS <tunnph25693@fpt.edu.vn>
2024-01-11 22:27:36 +0700

    Sua lai thong ke

--- frontend/src/controller/thong-ke/hienThiThongKeControler.js
+++ frontend/src/controller/thong-ke/hienThiThongKeControler.js
@@ -39,7 +39,7 @@ window.hienThiThongKeController = function ($http, $scope, $routeParams) {
         $scope.chartLabels = $scope.listThongKe.map(function (item) {
           return moment()
             .month(item.thang - 1)
-            .year(2023)
+            .year(item.nam)
             .format("MM/YYYY");
         });
         var ctx = document.getElementById("myChart").getContext("2d");
